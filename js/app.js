ebcdde2047f283af4ffb31a68817d1889ac2c307
mr.loki <abc@gmail.com>
2024-01-28 10:29:21 +0630

    header done & selection start

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("")
+let bar = document.querySelector("#bar");
+let navLink =document.querySelector(".nav-links");
+
+bar.addEventListener("click",function(){
+    // console.log("hi");
+    navLink.classList.toggle("show");
+    bar.classList.toggle("fa-times");
+});
+
