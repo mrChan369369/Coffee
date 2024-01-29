08bd8d2032890c89f1ddaacc49a416fd02febe75
mr.loki <abc@gmail.com>
2024-01-29 22:28:58 +0630

    test01

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 let bar = document.querySelector("#bar");
 let navLink =document.querySelector(".nav-links");
+let bannerCard =document.querySelector(".banner-card");
+
 
 bar.addEventListener("click",function(){
     // console.log("hi");
     navLink.classList.toggle("show");
     bar.classList.toggle("fa-times");
+    // bannerCard.style.display ="none"   //first way...
+    bannerCard.classList.toggle("hide");  //Sec logic...
+
 });
 
